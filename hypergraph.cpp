512ff8020f8d7b2023fb1f089b074097b18d5564
evan1590 <evanc1590@aol.com>
2015-02-02 14:29:55 -0500

    working on inserting
    it seems more like and adj list not matrix might need to redo

--- hypergraph.cpp
+++ hypergraph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ public:
 	Node* next;
 
 	string row;
+	vector<string> column;
 };
 
 Node::Node(string vertex)
@@ -28,7 +30,65 @@ public:
 
 	void insertNode(string);
 	void insertHyperEdge(string, string);
+	bool searchHypergraph(string);
 	void printHypergraph(string);
 	
 };
 
+Hypergraph::Hypergraph()
+{
+	root = NULL;
+}
+
+void Hypergraph::insertNode(string rows)
+{
+	if (root == NULL)
+	{
+		root = new Node(rows);
+		cout<< "Root is: " << root->rows << endl;
+	}
+	else
+	{
+		bool looksee = searchHypergraph(rows);
+		if (looksee == false)
+		{
+			Node* currentNode = root;
+			while(currentNode->next != NULL)
+			{
+				currentNode = currentNode->next;
+			}
+			currentNode->next = new Node(rows);
+		}
+	}
+}
+
+void Hypergraph::insertHyperEdge(string vertex, string edge);
+{
+	bool looksee = searchHypergraph(edge);
+	if (looksee == true)
+	{
+		bool NotHereSee = true;
+		Node* currentNode = root;
+		while(currentNode->next != NULL && NotHereSee)
+		{
+			if (vertex != currentNode->row)
+			{
+				currentNode = currentNode->next;
+			}
+			else
+			{
+				NotHereSee = false;
+			}
+		}
+		if (NotHereSee == false)
+		{
+			for (std::vector<string>::iterator i = currentNode->column.begin(); i != currentNode->column.end(); ++i)
+			{
+				if (currentNode->column[i] == edge)
+				{
+					break
+				}
+			}
+		}
+	}
+}
