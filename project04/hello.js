982e439e660ece9cf86610036902cebafae5494d
Evan Chessman <evanc1590@aol.com>
2015-05-06 11:26:59 -0400

    made changes to project 4 it was the wrong files

--- project04/hello.js
+++ project04/hello.js
@@ -5,14 +5,137 @@ var client = arDrone.createClient();
 
 client.takeoff();
 
+// client
+//   .after(5000, function() {
+//     this.clockwise(0.5);
+//   })
+//   .after(3000, function() {
+//     this.animate('flipLeft', 15);
+//   })
+//    .after(3000, function() {
+//     this.animate('doublePhiThetaMixed', 15);
+//   })
+//   .after(5000, function() {
+//     this.stop();
+//     this.land();
+//   });
+
+// client
+//   .after(1000, function() {
+//      this.stop();
+//    })  //Hover
+//   .after(5000, function() {
+//     this.front(0.3);
+//   })
+//   .after(4000, function() {
+//     this.stop();
+//   })
+//   .after(5000, function() {
+//     this.back(0.3);
+//   })
+//   .after(4000, function() {
+//     this.animate('flipLeft', 15);
+//   })
+//   .after(4000, function() {
+//     this.stop();
+//     this.land();
+//   });
+
+// client
+//   .after(1000, function() {
+//      this.stop();
+//    })  //Hover
+//   .after(5000, function() {
+//     this.up(0.5);
+//   })
+//   .after(5000, function() {
+//     this.front(0.3);
+//   })
+//   .after(5000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//    .after(3000, function() {
+//     this.front(0.3);
+//   })
+//    .after(5000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//    .after(3000, function() {
+//     this.front(0.3);
+//   })
+//    .after(5000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//   .after(3000, function() {
+//     this.stop();
+//     this.land();
+//   });
+
+// client
+//   .after(1000, function() {
+//      this.stop();
+//   })  //Hover
+//   .after(5000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//   .after(1750, function() {
+//        this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.front(0.3);
+//   })
+//   .after(5000, function() {
+//      this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//   .after(1750, function() {
+//        this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.front(0.3);
+//   })
+//   .after(5000, function() {
+//      this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//   .after(1750, function() {
+//        this.stop();
+//   })  //Hover
+//     .after(3000, function() {
+//     this.up(0.5);
+//   })
+//   .after(3000, function() {
+//     this.front(0.3);
+//   })
+//   .after(5000, function() {
+//        this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.counterClockwise(0.5);
+//   })
+//   .after(1750, function() {
+//        this.stop();
+//   })  //Hover
+//   .after(3000, function() {
+//     this.front(0.3);
+//   })
+//   .after(3000, function() {
+//      this.stop(); // to account for wind
+//   })  //Hover
+//   .after(5000, function() {
+//     this.stop();
+//     this.land();
+//   });
+
 client
-  .after(5000, function() {
-    this.clockwise(0.5);
+   .after(5000, function() {
+    this.animate('turnaround', 30);
   })
-  .after(3000, function() {
-    this.animate('flipLeft', 15);
-  })
-  .after(1000, function() {
+  .after(10000, function() {
     this.stop();
     this.land();
   });
